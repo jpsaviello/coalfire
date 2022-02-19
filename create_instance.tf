c904f62ce25f8f6a9db5db42b4f6d4e38ff33aae
jpsaviello <47954689+jpsaviello@users.noreply.github.com>
2022-02-18 21:44:30 -0500

    Update create_instance.tf

--- create_instance.tf
+++ create_instance.tf
@@ -7,7 +7,7 @@ resource "aws_volume_attachment" "ebs_att" {
 //Went with the AWS Linux AMI and installed an EBS volume for storage
 resource "aws_instance" "redhat" {
   ami               = "ami-0ff8a91507f77f867"
-  subnet_id         = "subnet-00cf0b2454e91e764"
+  subnet_id         = "subnet-0a06017c0124c56f2"
   availability_zone = "us-east-1b"
   instance_type     = "t2.micro"
 
