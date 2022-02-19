3fa69f02a9c6d39fd191fcbaf832bf7232d201e1
jpsaviello <47954689+jpsaviello@users.noreply.github.com>
2022-02-18 21:27:47 -0500

    Update aws_alb.tf
    updating alb

--- aws_alb.tf
+++ aws_alb.tf
@@ -1,52 +1,24 @@
-
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-
-  name = "my-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id             = "vpc-0d9df4ae74bb666c6"
-  subnets            = ["subnet-0452ff73f215eb78f", "subnet-02b91e282011afd0e"]
-
-
-   target_groups = [
-    {
-      name_prefix      = "pref-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = [
-        {
-          target_id = "i-04b6c1775328c93d7"
-          port = 80
-        }
-      ]
-    }
-  ]
-
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-    https_listeners = [
-    {
-      port               = 443
-      protocol           = "HTTPS"
-      certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-      target_group_index = 0
-    }
-  ]
-
-
-  tags = {
-    Environment = "Test"
-  }
+resource "aws_vpc" "main" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
+resource "aws_security_group" "elb_sg" {
+  vpc_id      = aws_vpc.main.id
+ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    description = "HTTP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
