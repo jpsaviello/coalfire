4b16478e6a300d79306091baa9b46aa5083251f0
jpsaviello <47954689+jpsaviello@users.noreply.github.com>
2022-02-18 23:01:01 -0500

    Update s3_bucket.tf
    Updating s3 bucket

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,4 +1,34 @@
 resource "aws_s3_bucket" "this" {
 
   force_destroy                        = var.force_destroy
+
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+    bucket = aws_s3_bucket.this.id
+rule {
+ 
+    id      = "log"
+    status = "Enabled"
+    prefix  = "Logs/"
+
+    expiration {
+      days = 90
+    }
+    
+
+}
+
+rule {
+ 
+    id      = "images"
+    status = "Enabled"
+    prefix  = "Images/"
+
+    transition {
+        days = 90
+        storage_class = "GLACIER"
+    }
+
+}
 }
