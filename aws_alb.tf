8b32fd3368e8841f602823d40aea22c550d20b8e
jpsaviello <47954689+jpsaviello@users.noreply.github.com>
2022-02-18 21:56:14 -0500

    Update aws_alb.tf
    Updating alb

--- aws_alb.tf
+++ aws_alb.tf
@@ -22,3 +22,43 @@ egress {
   }
 
 }
+
+resource "aws_lb_target_group" "TG-tf" {
+  name     = "Demo-TargetGroup-tf"
+  depends_on = ["aws_vpc.main"]
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  health_check {
+    interval            = 70
+    path                = "/index.html"
+    port                = 80
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 60 
+    protocol            = "HTTP"
+    matcher             = "200,202"
+  }
+}
+
+resource "aws_lb" "ALB-tf" {
+  name              = "Demo-ALG-tf"
+  internal           = false
+  load_balancer_type = "application"
+ # security_groups  = [aws_security_group.elb_sg.id]
+  subnets          = ["subnet-0a7a482e23da1cd2c", "subnet-09066cef4e2233bd3"]       
+  tags = {
+        name  = "Demo-AppLoadBalancer-tf"
+        Project = "demo-assignment"
+       }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.ALB-tf.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.TG-tf.arn
+  }
+}
